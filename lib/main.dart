3a6153a40121eb737ce4ac83de9f4fa216e6a63a
Anikshakya <aniklinkin@gmail.com>
2022-09-09 15:20:28 +0545

    added flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,15 @@ import 'package:grid_pluto/pluto_grid.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: const FirebaseOptions(
-    //   apiKey: "AIzaSyCfJF3hLHRsdmVcLRaUQIDbHzjCVd5-hsY",
-    //   authDomain: "pluto-grid.firebaseapp.com",
-    //   projectId: "pluto-grid",
-    //   storageBucket: "pluto-grid.appspot.com",
-    //   messagingSenderId: "424888225783",
-    //   appId: "1:424888225783:web:ed2e68bc04bfea1d7c68f2",
-    //   measurementId: "G-RN9TSNW5LB"
-    // ),
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCfJF3hLHRsdmVcLRaUQIDbHzjCVd5-hsY",
+      authDomain: "pluto-grid.firebaseapp.com",
+      projectId: "pluto-grid",
+      storageBucket: "pluto-grid.appspot.com",
+      messagingSenderId: "424888225783",
+      appId: "1:424888225783:web:ed2e68bc04bfea1d7c68f2",
+      measurementId: "G-RN9TSNW5LB"
+    ),
   );
   runApp(const MyApp());
 }
